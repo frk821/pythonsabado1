364ba8693484e139602c7908070ed157e8595e5c
209 <209@academicos.cesde.edu.co>
2022-08-13 13:07:54 -0500

    NTS2- Se crea archivo con datos de entrada y de salida y se crea archivo con condicional

---
+++ main.py
@@ -0,0 +1,35 @@
+#Hola mundo con Python
+
+
+#Salida por consola
+'''
+    print("Hola mundo")
+    print('Hola mundo Frank')
+'''
+
+#Entradas o Variables
+#Datos primitivos
+dato = None
+nombre = 'Frank'
+edad = 40
+estatura = 1.75
+hinchaDelVerde = True
+
+#Salida por teclado
+
+print("hola ",nombre, " Tu edad es ",edad)
+print(f'hola {nombre} tu edad es: {edad}')
+
+#Recibir datos por teclado
+ciudad = input('Digita la  ciudad donde vives: ')
+print(f'La ciudad donde usted vive: {ciudad}')
+
+#Recibir datos numericos por teclado
+numero1 = int (input("Digita un numero: "))
+numero2 = int (input("Digita un numero: "))
+print(f'El numero1 = {numero1} y numero2 = {numero2}')
+
+#Operadores Aritméticos
+#(+ - * / %)
+suma = numero1+numero2
+print(f'la suma es: {suma}')
---
+++ reto1.py
@@ -0,0 +1,11 @@
+numero = int(input("Ingrese un numero entero: "))
+
+resultado = numero % 5
+print(f'El residuo es: {resultado}')
+
+#Condicional simple en Python
+if (resultado == 0):
+    print("Es multiplo de 5")
+else: 
+    print("No es multiplo de 5")
+print("fin del programa")    
